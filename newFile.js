93af74efeb0bdced86931b4bf286f517f7cd7563
Olawale Bashiru <qbashy@gmail.com>
2021-04-27 05:58:27 +0100

    added more javascript

--- newFile.js
+++ newFile.js
@@ -1,3 +1,7 @@
 //TODO: Test creating new file & writing into it from the command line using 'vi' command.
 //
 const division = (a, b) => a / b;
+const addNo = function (a, b) {
+    return a + b;
+};
+const multiplyNo = (a, b) => a * b;
