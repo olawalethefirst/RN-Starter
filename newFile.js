f7847c627416d1023fec895fdd33690d0a7d2c73
Olawale Bashiru <66824020+olawalethefirst@users.noreply.github.com>
2021-04-27 08:08:31 +0100

    Feature xyz (#2)
    * add some javascript
    
    * added more javascript
    
    * Update newFile.js
    
    * Update newFile.js
    
    * resolved conflict

--- newFile.js
+++ newFile.js
@@ -1,3 +1,8 @@
 //TODO: Test creating new file & writing into it from the command line using 'vi' command.
 //
-const division = (a, b) => a / b
+const division = (a, b) => a / b;
+const multiplyNo = (a, b) => a * b;
+const addNo = function (a, b) {
+    return a + b;
+};
+const subtractNo = (a, b) => a - b;
