580a9cc764ebbc6f95aa792919e4f817ec891e04
Olawale Bashiru <qbashy@gmail.com>
2021-04-27 06:46:45 +0100

    resolved conflict

--- newFile.js
+++ newFile.js
@@ -2,5 +2,7 @@
 //
 const division = (a, b) => a / b;
 const multiplyNo = (a, b) => a * b;
-const addNo = function(a, b) {return a +b}
-const subtractNo = (a, b) => a -b
+const addNo = function (a, b) {
+    return a + b;
+};
+const subtractNo = (a, b) => a - b;
